c6e339635dd3a992ba85f42d27a9db9f43acef77
Gautierpicon <gautierpicon@proton.me>
2026-02-18 16:50:16 +0100

    add a gooffy profile picture (ChildishDog)

--- src/lib/utils/profilePictures.js
+++ src/lib/utils/profilePictures.js
@@ -2,12 +2,15 @@ import BabyShrek from '$lib/assets/ProfilePictures/BabyShrek.png';
 import ConfusedMonkey from '$lib/assets/ProfilePictures/ConfusedMonkey.png';
 import PepeTheFrog from '$lib/assets/ProfilePictures/PepeTheFrog.png';
 import spooderman from '$lib/assets/ProfilePictures/spooderman.png';
+import ChildishDog from '$lib/assets/ProfilePictures/ChildishDog.png';
+
 
 const profilePictures = {
 	'BabyShrek.png': BabyShrek,
 	'ConfusedMonkey.png': ConfusedMonkey,
 	'PepeTheFrog.png': PepeTheFrog,
-	'spooderman.png': spooderman
+	'spooderman.png': spooderman,
+	'ChildishDog.png': ChildishDog
 };
 
 const pictureNames = Object.keys(profilePictures);
